3d29ad520e7ea9d8b66e1b4a0b8771018df4bd5f
Thiago P12 <thiagopinheiro3110@gmail.com>
2023-12-04 21:07:14 -0300

    Projeto proposto pelo Felipe

---
+++ index.js
@@ -0,0 +1,22 @@
+
+let nome_heroi = input("Digite o nome do herói:" )
+let xpHeroi = parseInt(input("Digite a quantidade de xp do heroi"));
+let nivelHeroi = "";
+if (xpHeroi < 1000) {
+    nivelHeroi = "Ferro";
+} else if (xpHeroi >= 1001 && xpHeroi <= 2000) {
+    nivelHeroi = "Bronze";
+} else if (xpHeroi >= 2001 && xpHeroi <= 5000) {
+    nivelHeroi = "Prata";
+} else if (xpHeroi >= 6001 && xpHeroi <= 7000) {
+    nivelHeroi = "Ouro";
+} else if (xpHeroi >= 7001 && xpHeroi <= 8000) {
+    nivelHeroi = "Platina";
+} else if (xpHeroi >= 8001 && xpHeroi <= 9000) {
+    nivelHeroi = "Ascendente";
+} else if (xpHeroi >= 9001 && xpHeroi <= 10000) {
+    nivelHeroi = "Imortal";
+} else {
+    nivelHeroi = "Radiante";
+}
+console.log("O Herói de nome " + nome_heroi + " está no nível de " + nivelHeroi);
